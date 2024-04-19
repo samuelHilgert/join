b00aa23c33e716863febf96e3a0bf025e1ef7cfb
Oliver Drexler <oliverdrexler@web.de>
2024-04-19 14:05:43 +0200

    Update contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -7,6 +7,24 @@ let contacts = [
     },
     {
         name: "Anna Müller",
+        mail: "anna.mueller@strive.com",
+        phone: "+43 789 878 566",
+        color: ""
+    },
+    {
+        name: "John Walker",
+        mail: "jw@dreamlogistics.com",
+        phone: "+49 64865155",
+        color: ""
+    },
+    {
+        name: "Frank Groß",
+        mail: "groß@frank.de",
+        phone: "+49 668463546",
+        color: ""
+    },
+    {
+        name: "Anna Müller",
         mail: "anna.mueller@example.com",
         phone: "+43 789 878 566",
         color: ""
