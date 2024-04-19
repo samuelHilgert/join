66a534faa09af2792591a2802572735f6fb2c775
Oliver Drexler <oliverdrexler@web.de>
2024-04-19 09:54:36 +0200

    ongoing tests for sidebar js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
  */
 async function init() {
     await includeHTML();
+    setBackgroundcolorSidebarLinks();
     updateHTML();
     }
 
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,25 +1,22 @@
 // TEST für sidebar Farbwechsel bei click auf Menü-Element
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Suchen Sie nach dem Element mit der ID 'sidebar-sum'
-    var sidebarSum = document.getElementById("sidebar-sum");
 
-    // Überprüfen Sie, ob das Element existiert, bevor Sie die Event-Handler anhängen
-    if (sidebarSum) {
-        // Fügen Sie einen Event-Listener hinzu, um den :active-Status beim Klicken beizubehalten
-        sidebarSum.addEventListener("click", function() {
-            // Fügen Sie die Klasse .sidebar-bg-focus hinzu
-            this.classList.add("sidebar-bg-focus");
+function setBackgroundcolorSidebarLinks() {
+    let url = window.location.pathname;
+    let sidebarLinks = getSidebarLinks();
 
-            // Speichern Sie den :active-Status im localStorage
-            localStorage.setItem("sidebarActive", "sidebar-sum");
-        });
-    }
+    sidebarLinks.forEach((sidebarLink) => {
+        let href = sidebarLink.getAttribute('href');
+        if (url === href) {
+            sidebarLink.classList.add('sidebar-bg-focus');
+        } else {
+            sidebarLink.classList.remove('sidebar-bg-focus');
+        }
+    });
+}
 
-    // Überprüfen Sie, ob der :active-Status im localStorage gespeichert ist und setzen Sie ihn beim Laden der Seite zurück
-    var activeLink = localStorage.getItem("sidebarActive");
-    if (activeLink) {
-        document.getElementById(activeLink).classList.add("sidebar-bg-focus");
-    }
-});
+
+function getSidebarLinks() {
+    return document.querySelectorAll('sidebar-links .sidebar-menu');
+}
