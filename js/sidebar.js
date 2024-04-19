95fe920e02348e44c67717bdc06a9a1c18e484ac
Oliver Drexler <oliverdrexler@web.de>
2024-04-19 09:03:37 +0200

    testing some js functions for sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,26 @@
 // TEST für sidebar Farbwechsel bei click auf Menü-Element
 
 
-function changeBackgroundcolor() {
-    document.getElementById('menu2');
-    
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var sidebarLinks = document.querySelectorAll(".sidebar-menu");
+
+    sidebarLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            // Speichern Sie den Index des geklickten Links im localStorage
+            localStorage.setItem("activeLink", Array.from(sidebarLinks).indexOf(this));
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var activeLinkIndex = localStorage.getItem("activeLink");
+    if (activeLinkIndex !== null) {
+        var sidebarLinks = document.querySelectorAll(".sidebar-menu");
+        sidebarLinks.forEach(function(link, index) {
+            if (index.toString() === activeLinkIndex) {
+                link.classList.add("active");
+            }
+        });
+    }
+});
