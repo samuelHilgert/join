1f63838ca1944965c62976566fb950078013b218
Oliver Drexler <oliverdrexler@web.de>
2024-04-20 08:27:46 +0200

    added css

--- js/contacts.js
+++ js/contacts.js
@@ -1,32 +1,14 @@
 let contacts = [
     {
-        name: "Max Baumgart",
-        mail: "max@baumgart.de",
-        phone: "+49 12 123 456",
-        color: ""
-    },
-    {
         name: "Anna Müller",
         mail: "anna.mueller@strive.com",
         phone: "+43 789 878 566",
         color: ""
     },
     {
-        name: "James Walker",
-        mail: "jw@dreamlogistics.com",
-        phone: "+49 64865155",
-        color: ""
-    },
-    {
-        name: "Frank Groß",
-        mail: "groß@frank.de",
-        phone: "+49 668463546",
-        color: ""
-    },
-    {
-        name: "Leonie Maier",
-        mail: "leonie.maier@abc-construct.com",
-        phone: "+49 596 487 12",
+        name: "Bernd Hofmann",
+        mail: "hofmann@ib-bank.com",
+        phone: "+49 647 289 145",
         color: ""
     },
     {
@@ -36,9 +18,27 @@ let contacts = [
         color: ""
     },
     {
-        name: "Paul Lee",
-        mail: "lee@lee-enterprises.com",
-        phone: "+97 947621654",
+        name: "Frank Groß",
+        mail: "groß@frank.de",
+        phone: "+49 668463546",
+        color: ""
+    },
+    {
+        name: "James Walker",
+        mail: "jw@dreamlogistics.com",
+        phone: "+49 64865155",
+        color: ""
+    },
+    {
+        name: "Leonie Maier",
+        mail: "leonie.maier@abc-construct.com",
+        phone: "+49 596 487 12",
+        color: ""
+    },
+    {
+        name: "Max Baumgart",
+        mail: "max@baumgart.de",
+        phone: "+49 12 123 456",
         color: ""
     },
     {
@@ -48,9 +48,9 @@ let contacts = [
         color: ""
     },
     {
-        name: "Bernd Hofmann",
-        mail: "hofmann@ib-bank.com",
-        phone: "+49 647 289 145",
+        name: "Paul Lee",
+        mail: "lee@lee-enterprises.com",
+        phone: "+97 947621654",
         color: ""
     },
     {
