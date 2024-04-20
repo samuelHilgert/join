57e719587120ee2660c647891edd35b1e5f57cf5
Rio741 <rio96@gmx.de>
2024-04-20 22:43:36 +0200

    dropdown add task

--- js/add-task.js
+++ js/add-task.js
@@ -1,4 +1,11 @@
 function openDropdownContacts() {
   let Dropdownmenu = document.getElementById("inputfield-dropdown");
   let dropdownArrow = document.getElementById("dropdown-arrow");
+  let dropdownDiv = document.getElementById("dropdown-div");
+  dropdownDiv.style.display = "flex"; // "flex" als Zeichenkette übergeben
+
+  for (let i = 0; i < contacts.length; i++) {
+    const element = contacts[i];
+    dropdownDiv.innerHTML += `<div>hallo</div>`;
+  }
 }
