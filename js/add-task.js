3628e23bc4dde6111b8d2effe9d1145ede2623a0
Rio741 <rio96@gmx.de>
2024-04-21 10:08:39 +0200

    create new style

--- js/add-task.js
+++ js/add-task.js
@@ -2,17 +2,21 @@ function openDropdownContacts() {
   let Dropdownmenu = document.getElementById("inputfield-dropdown");
   let dropdownArrow = document.getElementById("dropdown-arrow");
   let dropdownDiv = document.getElementById("dropdown-div");
-  dropdownDiv.style.display = "flex"; // "flex" als Zeichenkette übergeben
+  dropdownDiv.style.display =
+    dropdownDiv.style.display === "flex" ? "none" : "flex";
 
   for (let i = 0; i < contacts.length; i++) {
     const element = contacts[i];
-    dropdownDiv.innerHTML += ` <div class="contact-small">
-    <div class="contact-circle d_f_c_c">
-        <div class="contact-circle-letters">FG</div>
-    </div>
-    <div class="contact-name-mail">
+    dropdownDiv.innerHTML += `
+    <div class="parting-line-dropdown"></div>
+    <div class="dropdown-contact">
+      <div class="contact-circle d_f_c_c">
+        <div class="contact-circle-letters">AM</div>
+      </div>
+      <div class="contact-name-mail">
         <div class="contact-name">${element.name}</div>
+      </div>
     </div>
-</div>`;
+    `;
   }
 }
