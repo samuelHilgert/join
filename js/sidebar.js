80b902d5b0460b7df95c53c5762b9fc7b43273e6
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-04-23 15:40:35 +0200

    added js function sidebarlink bgcolor

--- js/sidebar.js
+++ js/sidebar.js
@@ -19,13 +19,13 @@ function getSidebarLinks() {
     return document.querySelectorAll('.sidebar-links .sidebar-menu');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Hier wird der Ereignislistener hinzugefügt, sobald das DOM geladen ist
     let sidebarLinks = document.querySelectorAll('.sidebar-menu'); // Auswahl aller Sidebar-Links
 
     // Iteration über alle Sidebar-Links
     sidebarLinks.forEach((link) => {
-        link.addEventListener('click', function(event) {
+        link.addEventListener('click', function (event) {
             setBackgroundcolorSidebarLinks(); // Aufruf der Funktion zum Setzen der Hintergrundfarbe
         });
     });
@@ -43,13 +43,39 @@ function setBackgroundcolorSidebarLinks() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     setBackgroundcolorSidebarLinks(); // Setzen der Hintergrundfarbe beim Laden der Seite
 
     // Ereignislistener für Klicks auf die Sidebar-Links hinzufügen
     document.querySelectorAll('.sidebar-links .sidebar-menu').forEach((link) => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function () {
             setBackgroundcolorSidebarLinks(); // Setzen der Hintergrundfarbe nach einem Klick
         });
     });
 });
+
+function getCurrentlySidebarLink() {
+    let currentUrl = window.location.href;
+    let position;
+    if (currentUrl.indexOf('summary') !== -1) {
+        position = 0;
+        changeBgColorSidebarLink(position);
+    }
+    if (currentUrl.indexOf('add-task') !== -1) {
+        position = 1;
+        changeBgColorSidebarLink(position);
+    }
+    if (currentUrl.indexOf('board') !== -1) {
+        position = 2;
+        changeBgColorSidebarLink(position);
+    }
+    if (currentUrl.indexOf('contacts') !== -1) {
+        position = 3;
+        changeBgColorSidebarLink(position);
+    }
+}
+
+function changeBgColorSidebarLink(position) {
+    let sidebarMenu = document.getElementsByClassName('sidebar-menu')[position]; // Zugriff auf das erste Element in der Sammlung
+    sidebarMenu.style.backgroundColor = 'rgba(9, 25, 49, 1)';
+}
