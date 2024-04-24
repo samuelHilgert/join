c9940fd7f62addc85fe426de1b77b9544da5c244
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-04-24 14:20:20 +0200

    update register.js

--- js/register.js
+++ js/register.js
@@ -2,7 +2,6 @@ let users = [];
 let registerSuccess = false;
 let checkbox = false;
 let emailExist = false;
-let messageDisplayTime = 2000;
 
 
 /**
@@ -21,23 +20,24 @@ async function loadUsers() {
  * This function checks the individual requirements for successful registration
  * 
  */
-async function register() {
+function register() {
+    let registerBtn = document.getElementById('registerBtn');
     registerBtn.disabled = true; // registerBtn ist die id vom button Absenden
-    await checkboxClicked();
+    checkboxClicked();
 }
 
 /**
  * This function checks whether the checkbox was clicked by user
  * 
  */
-async function checkboxClicked() {
+function checkboxClicked() {
     if (signUpCheckbox.checked) {
         checkbox = true;
-        await checkEmailExist();
+        checkEmailExist();
     } else {
         showSignUpMessage();
-        await messageDisplay();
-        await hideMessageContainer();
+        messageDisplay();
+        hideMessageContainer();
         signUpErrorReset();
     }
 }
@@ -46,19 +46,19 @@ async function checkboxClicked() {
  * This function checks whether email already exist
  * 
  */
-async function checkEmailExist() {
+function checkEmailExist() {
     if (users.some(user => user.email === registerEmail.value)) {
         emailExist = true;
         registerEmail.value = '';
         showSignUpMessage();
-        await messageDisplay();
-        await hideMessageContainer();
+        messageDisplay();
+        hideMessageContainer();
         signUpErrorReset();
-       // setTimeout(messageDisplayNone, messageDisplayTime);
-      //  showSignUpMessage();
+        // setTimeout(messageDisplayNone, messageDisplayTime);
+        //  showSignUpMessage();
     }
     else {
-        await signUpPasswordsMatched();
+        signUpPasswordsMatched();
     }
 }
 
@@ -66,24 +66,24 @@ async function checkEmailExist() {
  * This function checks whether the two passwords match
  * 
  */
-async function signUpPasswordsMatched() {
+function signUpPasswordsMatched() {
     if (password.value === confirmPassword.value) {
         registerSuccess = true;
-        await pushUserData();
+        pushUserData();
         showSignUpMessage();
-        await messageDisplay();
-        await hideMessageContainer();
+        messageDisplay();
+        hideMessageContainer();
         resetSingUpForm();
         // showSignUpMessage();
-       // setTimeout(backToLogin, 800);
+        // setTimeout(backToLogin, 800);
     }
     else {
         showSignUpMessage();
-        await messageDisplay();
-        await hideMessageContainer();
+        messageDisplay();
+        hideMessageContainer();
         resetSingUpForm();
-       // setTimeout(messageDisplayNone, messageDisplayTime);
-       // showSignUpMessage();
+        // setTimeout(messageDisplayNone, messageDisplayTime);
+        // showSignUpMessage();
     }
 }
 
@@ -92,6 +92,7 @@ async function signUpPasswordsMatched() {
  * 
  */
 function signUpErrorReset() {
+    let registerBtn = document.getElementById('registerBtn');
     password.value = '';
     confirmPassword.value = '';
     registerBtn.disabled = false;
@@ -101,22 +102,22 @@ function signUpErrorReset() {
  * This function set message display to none
  * 
  */
-async function messageDisplay() {
+function messageDisplay() {
     let messageContainer = document.getElementById('messageContainer');
     let container = document.getElementById('messageFormSignUp');
     messageContainer.style.display = 'flex';
     container.style.display = 'flex';
     moveContainerIn(container);
-    setTimeout(function() {
+    setTimeout(function () {
         moveContainerOut(container);
     }, 2000);
 }
 
-async function hideMessageContainer() {
+function hideMessageContainer() {
     let messageContainer = document.getElementById('messageContainer');
     let container = document.getElementById('messageFormSignUp');
     // messageContainer.style.display = 'none';
-   // container.style.display = 'none';
+    // container.style.display = 'none';
 }
 
 /**
@@ -174,6 +175,7 @@ function showSignUpMessage() {
  * 
  */
 function resetSingUpForm() {
+    let registerBtn = document.getElementById('registerBtn');
     signUpName.value = '';
     registerEmail.value = '';
     password.value = '';
@@ -190,3 +192,5 @@ function resetSingUpForm() {
 function backToLogin() {
     window.location.href = './login.html?msg=Du hast dich erfolgreich registriert'; // queryParameter 
 }
+
+
