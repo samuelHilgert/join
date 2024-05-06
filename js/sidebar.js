102e9d82161e2849954c68d9fc9e6e00b914c808
Rio741 <rio96@gmx.de>
2024-05-06 17:47:38 +0200

    update sidebar script

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,15 @@
-// TEST FÜR SIDEBAR LINKS
+/**
+ * Sets the background color for the sidebar links based on the current page.
+ * If the link matches the current page, adds the class 'sidebar-bg-focus',
+ * otherwise removes this class.
+ */
 function setBackgroundcolorSidebarLinks() {
     let url = window.location.pathname;
     let sidebarLinks = getSidebarLinks();
-
     sidebarLinks.forEach((sidebarLink) => {
         let href = sidebarLink.getAttribute('href');
         if (url === href) {
             sidebarLink.classList.add('sidebar-bg-focus');
-            console.log('TEST');
         } else {
             sidebarLink.classList.remove('sidebar-bg-focus');
         }
@@ -18,18 +20,26 @@ function getSidebarLinks() {
     return document.querySelectorAll('.sidebar-links .sidebar-menu');
 }
 
+/**
+ * Adds event listeners to the sidebar menu links when the DOM content is loaded.
+ * When a sidebar menu link is clicked, it triggers the function to set the background color for sidebar menu links.
+ */
 document.addEventListener('DOMContentLoaded', function () {
-    // Hier wird der Ereignislistener hinzugefügt, sobald das DOM geladen ist
-    let sidebarLinks = document.querySelectorAll('.sidebar-menu'); // Auswahl aller Sidebar-Links
-
-    // Iteration über alle Sidebar-Links
+    let sidebarLinks = document.querySelectorAll('.sidebar-menu');
     sidebarLinks.forEach((link) => {
         link.addEventListener('click', function (event) {
-            setBackgroundcolorSidebarLinks(); // Aufruf der Funktion zum Setzen der Hintergrundfarbe
+            setBackgroundcolorSidebarLinks();
         });
     });
 });
 
+/**
+ * Redirects the user to legal or summary pages based on the current URL.
+ * If the current URL contains 'legal-notice.html?external', redirects to 'privacy-policy.html?external'.
+ * If the current URL contains 'privacy-policy.html?external', redirects to 'login.html'.
+ * If the current URL contains 'legal-notice.html', redirects to 'privacy-policy.html'.
+ * If the current URL contains 'privacy-policy.html', redirects to 'summary.html'.
+ */
 function backToLegalOrSummary() {
     let currentUrl = window.location.href;
     if (currentUrl.indexOf('legal-notice.html?external') !== -1) {
@@ -43,8 +53,9 @@ function backToLegalOrSummary() {
     }
 }
 
-// TEST 2
-
+/**
+ * Sets the background color for the sidebar links based on the current page.
+ */
 function setBackgroundcolorSidebarLinks() {
     let currentPagePath = window.location.pathname;
     document.querySelectorAll('.sidebar-links .sidebar-menu').forEach((link) => {
@@ -53,69 +64,102 @@ function setBackgroundcolorSidebarLinks() {
     });
 }
 
+/**
+ * Adds event listeners to the sidebar menu links when the DOM content is loaded.
+ * Sets the background color for the sidebar links when the page is loaded.
+ * When a sidebar menu link is clicked, it triggers the function to set the background color for sidebar links.
+ */
 document.addEventListener('DOMContentLoaded', function () {
-    setBackgroundcolorSidebarLinks(); // Setzen der Hintergrundfarbe beim Laden der Seite
-
-    // Ereignislistener für Klicks auf die Sidebar-Links hinzufügen
+    setBackgroundcolorSidebarLinks();
     document.querySelectorAll('.sidebar-links .sidebar-menu').forEach((link) => {
         link.addEventListener('click', function () {
-            setBackgroundcolorSidebarLinks(); // Setzen der Hintergrundfarbe nach einem Klick
+            setBackgroundcolorSidebarLinks();
         });
     });
 });
 
-function getCurrentlySidebarLink() {
-    let currentUrl = window.location.href;
-    let position;
-    if (currentUrl.indexOf('summary') !== -1) {
-        position = 0;
+/**
+ * Sets the background color for the sidebar link based on the current URL.
+ * @param {string} currentUrl The URL of the current page.
+ */
+function setSidebarLinkBackground(currentUrl) {
+    const keywords = ['summary', 'add-task', 'board', 'contacts'];
+    let position = keywords.findIndex(keyword => currentUrl.includes(keyword));
+    if (position !== -1) {
         changeBgColorSidebarLink(position);
-    }
-    if (currentUrl.indexOf('add-task') !== -1) {
-        position = 1;
-        changeBgColorSidebarLink(position);
-    }
-    if (currentUrl.indexOf('board') !== -1) {
-        position = 2;
-        changeBgColorSidebarLink(position);
-    }
-    if (currentUrl.indexOf('contacts') !== -1) {
-        position = 3;
-        changeBgColorSidebarLink(position);
-    }
-    if (currentUrl.indexOf('external') !== -1) {
-        let sidebarLinks = document.getElementById('sidebarLinks');
-        sidebarLinks.style.display = 'none';
-    }
-    if (currentUrl.indexOf('privacy-policy') !== -1) {
-        let legalSidebar = document.getElementsByClassName('sidebar-legal-element')[0];
-        legalSidebar.style.backgroundColor = 'rgba(9, 25, 49, 1)';
-        let headerSymbols = document.getElementById('headerSymbols');
-        headerSymbols.style.display = 'none';
-    }
-    if (currentUrl.indexOf('legal-notice') !== -1) {
-        let legalSidebar = document.getElementsByClassName('sidebar-legal-element')[1];
-        legalSidebar.style.backgroundColor = 'rgba(9, 25, 49, 1)';
-        let headerSymbols = document.getElementById('headerSymbols');
-        headerSymbols.style.display = 'none';
     }
 }
 
+/**
+ * Handles the behavior of the sidebar links for external pages.
+ * If the current URL contains 'external', hides the sidebar links.
+ * @param {string} currentUrl The URL of the current page.
+ */
+function handleExternalLink(currentUrl) {
+    if (currentUrl.includes('external')) {
+        let sidebarLinks = document.getElementById('sidebarLinks');
+        sidebarLinks.style.display = 'none';
+    }
+}
+
+/**
+ * Handles the behavior of the sidebar links for legal pages.
+ * If the current URL contains 'privacy-policy', sets the background color for the legal sidebar.
+ * If the current URL contains 'legal-notice', sets the background color for the legal sidebar.
+ * @param {string} currentUrl The URL of the current page.
+ */
+function handleLegalPages(currentUrl) {
+    if (currentUrl.includes('privacy-policy')) {
+        setLegalSidebarBackground(0);
+    }
+    if (currentUrl.includes('legal-notice')) {
+        setLegalSidebarBackground(1);
+    }
+}
+
+/**
+ * Sets the background color and hides header symbols for a legal sidebar element.
+ * @param {number} index The index of the legal sidebar element.
+ */
+function setLegalSidebarBackground(index) {
+    let legalSidebar = document.getElementsByClassName('sidebar-legal-element')[index];
+    legalSidebar.style.backgroundColor = 'rgba(9, 25, 49, 1)';
+    let headerSymbols = document.getElementById('headerSymbols');
+    headerSymbols.style.display = 'none';
+}
+
+/**
+ * Checks the current URL and performs actions for sidebar links, external links, and legal pages.
+ * @param {string} currentUrl The URL of the current page.
+ */
+function getCurrentlySidebarLink() {
+    let currentUrl = window.location.href;
+    setSidebarLinkBackground(currentUrl);
+    handleExternalLink(currentUrl);
+    handleLegalPages(currentUrl);
+}
+
+/**
+ * Changes the background color for a specific sidebar menu link.
+ * @param {number} position The position of the sidebar menu link.
+ */
 function changeBgColorSidebarLink(position) {
     let sidebarMenu = document.getElementsByClassName('sidebar-menu')[position];
     sidebarMenu.style.backgroundColor = 'rgba(9, 25, 49, 1)';
 }
 
+/**
+ * Opens a legal link in the sidebar, considering if it's an external link.
+ * @param {string} link The link to open.
+ */
 function openSidebarLegalLink(link) {
     let currentUrl = window.location.href;
     if (currentUrl.indexOf('external') !== -1) {
         let url = `./${link}.html`;
         let targetUrl = url + '?external';
         window.location.href = targetUrl;
-    }
-    else {
+    } else {
         let targetUrl = `./${link}.html`;
         window.location.href = targetUrl;
     }
-
 }
