e0bab09d310947a957f5d39afe25c212c4316346
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-05-18 11:14:35 +0200

    updated storage

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,35 @@
-const STORAGE_TOKEN = 'MU3TF9XZZPG6WRHN43HDM4XULHV4L5L4J71N3YEH';
+// const STORAGE_TOKEN = 'MU3TF9XZZPG6WRHN43HDM4XULHV4L5L4J71N3YEH';        without firebase
 const STORAGE_URL = 'https://remotestoragejoin-default-rtdb.europe-west1.firebasedatabase.app/';
+
+
+/**
+ * This function sends user data from the login form (from the "users" array) to the remote server.
+ * @param {*} key - 
+ * @param {*} value - 
+ * @returns 
+ */
+/*
+async function setItem(key, value) {
+  const url = ${STORAGE_URL}/${key}.json;
+  return fetch(url, {
+    method: "PUT", // Use PUT to create or replace data at a specific location
+    body: JSON.stringify(value), // Just the value, no need for a payload object
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+} */
+
+
+/**
+ * This function loads user data from the remote server via the URL
+ * @param {*} key - 
+ * 
+ */
+async function getItem(key) {
+let responsive = (await fetch(STORAGE_URL)).json();
+console.log(responsive);
+}
 
 
 /**
@@ -7,11 +37,13 @@ const STORAGE_URL = 'https://remotestoragejoin-default-rtdb.europe-west1.firebas
  * 
  * 
  */
+/* without firebase
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
     .then(res => res.json());
-}
+} */ 
+
 
 
 /**
@@ -19,6 +51,7 @@ async function setItem(key, value) {
  * 
  * 
  */
+/* without firebase
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => {
@@ -27,4 +60,4 @@ async function getItem(key) {
             return res.data.value;
         } throw `Could not find data with key "${key}".`;
     });
-}
+}*/
